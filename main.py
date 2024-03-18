0e76c214ee418ca18a41eae8b9614124c469ae1c
Nicolas Trelles <nicolastrellesviz@gmail.com>
2024-03-18 01:34:43 -0500

    navbar + jinja

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template
 from flask_restful  import Api, Resource
-
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 #Instancia de Flask
 app = Flask(__name__)
 api = Api(app)
+#Config para que funcione CSRF tokens con WTForms
+app.config['SECRET_KEY'] = "My llave secreta :3"
 
+#Clase para pruebas
+class NamerForm(FlaskForm):
+    name = StringField('Indique su Nombre' , validators=[DataRequired()])
+    submit = SubmitField("Submit")
 class usuarios(Resource):
     def get(self):
         return {'data': 'Hola Mundo'}
@@ -36,6 +44,20 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template('500.html'), 500
+
+#Name page
+@app.route("/name",methods=['GET','POST'])
+def name():
+    name = None
+    form = NamerForm()
+    #Validando Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template("name.html",
+                           name = name,
+                           form = form)
+
 
 api.add_resource(usuarios,"/holamundo")
 if  __name__ == '__main__':
